db3babeb1361e5ee9d522841dbea078f0b7ce102
abdelwaheb bhs <abdulwahebbenhaj@gmail.com>
2024-08-25 22:52:32 +0100

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Hero from "@/components/Hero";
+
+export default function Home() {
+  return (
+    <main className="relative bg-black-100
+    flex justfy-center items-center flex-col
+    overflow-hidden mx-auto sm:px-10 px-5
+    ">
+      <div className="max-w-7xt w-full">
+        <Hero />
+      </div>
+    </main>
+    );
+}
