edd0c0fe468da6108d8f61b400de7c23fc4ec44d
omarprojects99 <omarbenhajsaid1@gmail.com>
2024-09-06 19:13:40 +0100

    final update v2

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -32,7 +32,7 @@ const Footer = () => {
             {
                 socialMedia.map((profile) =>(
                     <a key={profile.id} href={profile.link} target="_blank" rel="noopener noreferrer" className='w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-100'>
-                        <img src={profile.img} alt={profile.id} width={20} height={20} />
+                        <img src={profile.img}  width={20} height={20} />
                     </a>
 
                 ) )}
