5557ca2190f8dd171c9e5444e9bb13e1a3d3074d
omarprojects99 <omarbenhajsaid1@gmail.com>
2024-10-24 21:08:39 +0100

    soft

--- components/Data/index.ts
+++ components/Data/index.ts
@@ -8,7 +8,7 @@ export const navItems = [
   export const gridItems = [
     {
       id: 1,
-      title: "Passionate about shaping the future of cloud computing and software software development ",
+      title: "Passionate about shaping the future of cloud computing and software development ",
       description: "",
       className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
       imgClassName: "w-full h-full",
